4484bba057806cd2384bb538be4b5e61d6552f53
Goonawardana R N W <gtrxone@gmail.com>
2021-11-18 19:39:34 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Area of a Circle = " + AreaCalculator.area(0));
+        System.out.println("Area of a Rectangle = " + AreaCalculator.area(10 , 0.5));
+    }
+}
